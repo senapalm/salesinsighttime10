fef8e2655e8af01790f50df9d4c0800ba03182ca
eduardaecher <eduarda_echer@hotmail.com>
2026-06-03 14:23:04 -0300

    Criando a função que gera o dataset

---
+++ src/salesinsight.py
@@ -0,0 +1,8 @@
+#SalesInsight PY: Pipeline de Análise e Visualização de Dados de Vendas
+
+#GERANDO O DATASET DE DADOS BRUTOS
+from data_loader import gerar_dataset_vendas
+df_bruto = gerar_dataset_vendas()
+
+#PRÉ-VISUALIZAÇÃO DOS DADOS BRUTOS
+df_bruto.head()
