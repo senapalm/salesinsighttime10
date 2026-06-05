9ff6d7e2b836c3a7fd7f8f262e3304959ef7dce1
eduardaecher <eduarda_echer@hotmail.com>
2026-06-05 14:11:45 -0300

    Criando funções de geração de gráficos

--- src/salesinsight.py
+++ src/salesinsight.py
@@ -6,35 +6,36 @@
 # RF03 - Limpeza de Dados
 # RF13 - Limpeza com Regex
 # RF04 - Colunas Derivadas
+# RF05 - CÁLCULO DE MÉTRICAS AGREGADAS
 # RF07 - Estatísticas NumPy
-# RF08-RF11 - Análises e Visualizações
+# RF08- Análises e Visualizações
 # RF12 - Exportação CSV/JSON
 
 from inheritance import AnalisadorComProjecao
 from lambdas import processar_coluna
 
-#GERANDO O DATASET DE DADOS BRUTOS
+#RF01 - GERANDO O DATASET DE DADOS BRUTOS
 from data_loader import gerar_dataset_vendas
 df_bruto = gerar_dataset_vendas()
 
-#INSPEÇÃO INICIAL DOS DADOS BRUTOS
+#RF02 - INSPEÇÃO INICIAL DOS DADOS BRUTOS
 from analysis import inspecionar_dados
 inspecionar_dados(df_bruto)
 
 
-#LIMPEZA E TRATAMENTO DOS DADOS
+#RF03 - LIMPEZA E TRATAMENTO DOS DADOS
 from cleaning import limpar_dados
 df, relatorio_limpeza = limpar_dados(df_bruto)
 
-#TRANSFORMAÇÃO - CRIAÇÃO DE COLUNAS DERIVADAS
+#RF04 - TRANSFORMAÇÃO - CRIAÇÃO DE COLUNAS DERIVADAS
 from utils import criar_colunas_derivadas
 df = criar_colunas_derivadas(df)
 
-#CÁLCULO DE MÉTRICAS AGREGADAS
+#RF05 - CÁLCULO DE MÉTRICAS AGREGADAS
 from utils import calcular_metricas
 metricas = calcular_metricas(df)
 
-#SEGMENTAÇÃO DE CLIENTES POR NÍVEL DE GASTO
+#RF06 - SEGMENTAÇÃO DE CLIENTES POR NÍVEL DE GASTO
 from utils import segmentar_clientes
 segmentar_clientes = segmentar_clientes(df)
 
@@ -42,13 +43,17 @@ segmentar_clientes = segmentar_clientes(df)
 print("\n=== DATASET LIMPO E TRANSFORMADO ===")
 print(df.head())
 
-#ESTATISCAS BÁSICAS
+#RF07 - ESTATISCAS BÁSICAS COM NUMPY
 from utils import calcular_estatisticas_numpy
-calcular_estatisticas_numpy(df)
+stats_numpy = calcular_estatisticas_numpy(df)
+
+#RF08 - GERAÇÃO DE VISUALIZAÇÕES
+from visualization import gerar_visualizacoes
+gerar_visualizacoes(df, metricas)
 
 # LIMPEZA COM EXPRESSÕES REGULARES
 from utils import limpar_strings_com_regex
-df_bruto = limpar_strings_com_regex(df)
+df = limpar_strings_com_regex(df)
 
 # VERIFICAR RESULTADO
 print(df[["cliente", "cliente_limpo", "cliente_valido"]].head())
@@ -63,8 +68,7 @@ analisador.exibir_resumo()
 analisador.projetar_receita()
 
 
-
-df_bruto = processar_coluna(
+df = processar_coluna(
     df,
     "receita_total",
     lambda x: "Alto Valor" if x > 5000 else "Valor Normal")
@@ -81,9 +85,9 @@ os.makedirs("outputs", exist_ok=True)
 print("\n=== EXPORTAÇÃO DE RESULTADOS ===")
 print("Pasta de saída: outputs/")
 
-# Exportações (descomente quando as funções e dados existirem)
+# Exportações
 
-# exportar_csv(metricas)
-# exportar_json(stats_numpy)
+from utils import exportar_resultados
+exportar_resultados(metricas, segmentar_clientes, stats_numpy)
 
 print("Arquivos serão salvos na pasta outputs/")
